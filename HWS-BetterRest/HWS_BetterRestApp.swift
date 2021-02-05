2a72d6822c4bea9784ae5a49455fc562fca29653
Michael Bartee <michaelbartee@gmail.com>
2021-02-05 15:56:38 -0500

    Initial Commit

---
+++ HWS-BetterRest/HWS_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_BetterRestApp.swift
+//  HWS-BetterRest
+//
+//  Created by Michael Bartee on 2/5/21.
+//
+
+import SwiftUI
+
+@main
+struct HWS_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
